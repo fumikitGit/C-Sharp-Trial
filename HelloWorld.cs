931a2522b36543ca2ef6fc9760128a8f4a01816c
fumikitGit <36556220+fumikitGit@users.noreply.github.com>
2018-02-21 02:59:43 +0000

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,15 @@
+// URL: https://qiita.com/yuki451/items/c9c6a2b79ce96ae0d252
+
+using System;
+
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World");
+    }
+}
+
+// 実行で、"powershell"とタイプしてpowershellを起動。
+// コンパイル）C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe .\HelloWorld.cs
+// 実行）.\HellloWorld
